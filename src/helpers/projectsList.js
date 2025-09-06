9fae5463ed92585945952a7cbfd8df8846244d21
Andrey Potapov <potapov_andrey22@mail.ru>
2025-09-06 22:12:53 +0700

    helpers

---
+++ src/helpers/projectsList.js
@@ -0,0 +1,60 @@
+import project01 from "./../img/projects/01.jpg";
+import project02 from "./../img/projects/02.jpg";
+import project03 from "./../img/projects/03.jpg";
+import project04 from "./../img/projects/04.jpg";
+import project05 from "./../img/projects/05.jpg";
+import project06 from "./../img/projects/06.jpg";
+import project07 from "./../img/projects/07.jpg";
+
+const projects = [
+  {
+    title: "Streaming company site",
+    skills: "HTML, SCSS, JavaScript, JSX, Vite, Minista, BEM",
+    img: project01,
+    gitHubLink: "https://github.com/andreypo22/Stream-vibe",
+    previewLink: "https://stream-vibe-9qgb.onrender.com",
+  },
+  {
+    title: "Future Tech AI",
+    skills: "HTML, SCSS, JavaScript, BEM",
+    img: project02,
+    gitHubLink: "https://github.com/andreypo22/Future-Tech",
+    previewLink: "https://future-tech-ul87.onrender.com",
+  },
+  {
+    title: "React Cards App",
+    skills: "HTML, CSS module, React, Vite, JSON server",
+    img: project04,
+    gitHubLink: "https://github.com/andreypo22/react-app-course",
+    previewLink: "https://react-cards-app-i0jh.onrender.com",
+  },
+  {
+    title: "Netflix Clone site",
+    skills: "HTML, CSS, React, Vite, Firebase, TMDB (movie API)",
+    img: project07,
+    gitHubLink: "https://github.com/andreypo22/netflix-clone",
+  },
+  {
+    title: "Online clothing store",
+    skills: "HTML, SCSS, JavaScript, Gulp, Webpack, BEM",
+    img: project03,
+    gitHubLink: "https://github.com/andreypo22/Createx",
+    previewLink: "https://createx-app-7gw5.onrender.com",
+  },
+  {
+    title: "Creative studio page",
+    skills: "HTML, SCSS, JavaScript, Gulp, Webpack, BEM",
+    img: project05,
+    gitHubLink: "https://github.com/andreypo22/Creative_agansy_studio",
+    previewLink: "https://agansy-studio-app.onrender.com",
+  },
+  {
+    title: "W-wave radio site",
+    skills: "HTML, SCSS, JavaScript, Gulp, Webpack, BEM",
+    img: project06,
+    gitHubLink: "https://github.com/andreypo22/W-Wave_radio",
+    previewLink: "https://w-waveradio.onrender.com",
+  },
+];
+
+export { projects };
